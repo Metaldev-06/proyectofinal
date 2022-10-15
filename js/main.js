3ff8fb3fb27f85b555b8c50ad82192ab585b8f94
Fernando Diaz <70718713+Metaldev-06@users.noreply.github.com>
2022-10-15 00:31:44 -0300

    Version 0.2

--- js/main.js
+++ js/main.js
@@ -1,75 +1,102 @@
 let tecnologias = [
   {
-    img: "../../../../../assets/img/icons8-html-5.svg",
+    img: "../assets/img/icons8-html-5.svg",
     title: "HTML",
     description:
       "HTML es el lenguaje de marcado de contenido para la creación de páginas web, permite la creación de páginas web dinámicas y estándarizadas.",
   },
   {
-    img: "../../../../../assets/img/icons8-css3.svg",
+    img: "../assets/img/icons8-css3.svg",
     title: "CSS",
     description:
       "CSS es un lenguaje de presentación de una página web, que define las presentaciones de los elementos de una página web.",
   },
   {
-    img: "../../../../../assets/img/icons8-oreja.svg",
+    img: "../assets/img/icons8-oreja.svg",
     title: "Bootstrap",
     description:
       "Bootstrap es un framework css de desarrollo de páginas web para el desarrollo de aplicaciones web.",
   },
   {
-    img: "../../../../../assets/img/icons8-sass.svg",
+    img: "../assets/img/icons8-sass.svg",
     title: "Sass",
     description:
       "Sass es un lenguaje de diseño de páginas web para el desarrollo de aplicaciones web.",
   },
   {
-    img: "../../../../../assets/img/icons8-javascript.svg",
+    img: "../assets/img/icons8-javascript.svg",
     title: "JavaScript",
     description:
       "JavaScript es un lenguaje de programación de alto nivel que se utiliza en la creación de páginas web interactivas y de aplicaciones web.",
   },
   {
-    img: "../../../../../assets/img/icons8-typescript.svg",
+    img: "../assets/img/icons8-typescript.svg",
     title: "TypeScript",
     description:
       "TypesScript es un lenguaje de programación de código abierto desarrollado y mantenido por Microsoft. Es un superconjunto de JavaScript.",
   },
   {
-    img: "../../../../../assets/img/angular.svg",
+    img: "../assets/img/angular.svg",
     title: "Angular",
     description:
       "Angular es una plataforma de desarrollo de aplicaciones web y móviles multiplataforma que se basa en una arquitectura de componentes.",
   },
   {
-    img: "../../../../../assets/img/icons8-node-js.svg",
+    img: "../assets/img/icons8-node-js.svg",
     title: "NodeJS",
     description:
       "NodeJS es un entorno en tiempo de ejecución multiplataforma, de código abierto, para la capa del servidor basado en el lenguaje de programación JavaScript.",
   },
   {
-    img: "../../../../../assets/img/icons8-express-js.svg",
+    img: "../assets/img/icons8-express-js.svg",
     title: "Express",
     description:
       "Express es un entorno de trabajo para aplicaciones web para el programario Node.js, de código abierto y con licencia MIT. Se utiliza para desarrollar aplicaciones web y APIs.",
   },
   {
-    img: "../../../../../assets/img/php-svgrepo-com.svg",
+    img: "../assets/img/php-svgrepo-com.svg",
     title: "PHP",
     description:
       "PHP es un lenguaje de programación de alto nivel de uso general que se adapta especialmente al desarrollo web.",
   },
   {
-    img: "../../../../../assets/img/icons8-postgresql.svg",
+    img: "../assets/img/icons8-postgresql.svg",
     title: "PostgreSQL",
     description:
       "PostgreSQL es un sistema gestor de base de datos relacional de código abierto.",
   },
   {
-    img: "../../../../../assets/img/icons8-logo-de-mysql.svg",
+    img: "../assets/img/icons8-logo-de-mysql.svg",
     title: "MySQL",
     description:
       "MySQL es un sistema gestor de base de datos relacional de código abierto.",
+  },
+];
+
+let hobbies = [
+  {
+    img: "../assets/img/juego.svg",
+    title: "Jugar Videojuegos",
+  },
+  {
+    img: "../assets/img/musica.svg",
+    title: "Escuchar Música",
+  },
+  {
+    img: "../assets/img/tv.svg",
+    title: "Ver Series",
+  },
+  {
+    img: "../assets/img/programacion.svg",
+    title: "Programar",
+  },
+  {
+    img: "../assets/img/libro.svg",
+    title: "Leer Libros",
+  },
+  {
+    img: "../assets/img/correr.svg",
+    title: "Salir a Caminar",
   },
 ];
 
@@ -92,4 +119,22 @@ const mostrarDatos = (tecnologias) => {
   cards.appendChild(fragment);
 };
 
+const mostrarHobbies = (hobbies) => {
+  const cards = document.getElementById("cardContainerHobbies");
+  const templateCards = document.getElementById("templateCardHobbies").content;
+  const fragment = document.createDocumentFragment();
+
+  hobbies.map((item) => {
+    const clone = templateCards.cloneNode(true);
+
+    clone.querySelector("img").setAttribute("src", item.img);
+    clone.querySelector(".skills__card--content_title").textContent =
+      item.title;
+
+    fragment.appendChild(clone);
+  });
+  cards.appendChild(fragment);
+};
+
 mostrarDatos(tecnologias);
+mostrarHobbies(hobbies);
