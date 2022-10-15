2a4e10292bf7d9fd40e26020fb0b9092a9b327fb
Fernando Diaz <70718713+Metaldev-06@users.noreply.github.com>
2022-10-14 23:37:57 -0300

    Version 0.1

---
+++ js/main.js
@@ -0,0 +1,95 @@
+let tecnologias = [
+  {
+    img: "../../../../../assets/img/icons8-html-5.svg",
+    title: "HTML",
+    description:
+      "HTML es el lenguaje de marcado de contenido para la creación de páginas web, permite la creación de páginas web dinámicas y estándarizadas.",
+  },
+  {
+    img: "../../../../../assets/img/icons8-css3.svg",
+    title: "CSS",
+    description:
+      "CSS es un lenguaje de presentación de una página web, que define las presentaciones de los elementos de una página web.",
+  },
+  {
+    img: "../../../../../assets/img/icons8-oreja.svg",
+    title: "Bootstrap",
+    description:
+      "Bootstrap es un framework css de desarrollo de páginas web para el desarrollo de aplicaciones web.",
+  },
+  {
+    img: "../../../../../assets/img/icons8-sass.svg",
+    title: "Sass",
+    description:
+      "Sass es un lenguaje de diseño de páginas web para el desarrollo de aplicaciones web.",
+  },
+  {
+    img: "../../../../../assets/img/icons8-javascript.svg",
+    title: "JavaScript",
+    description:
+      "JavaScript es un lenguaje de programación de alto nivel que se utiliza en la creación de páginas web interactivas y de aplicaciones web.",
+  },
+  {
+    img: "../../../../../assets/img/icons8-typescript.svg",
+    title: "TypeScript",
+    description:
+      "TypesScript es un lenguaje de programación de código abierto desarrollado y mantenido por Microsoft. Es un superconjunto de JavaScript.",
+  },
+  {
+    img: "../../../../../assets/img/angular.svg",
+    title: "Angular",
+    description:
+      "Angular es una plataforma de desarrollo de aplicaciones web y móviles multiplataforma que se basa en una arquitectura de componentes.",
+  },
+  {
+    img: "../../../../../assets/img/icons8-node-js.svg",
+    title: "NodeJS",
+    description:
+      "NodeJS es un entorno en tiempo de ejecución multiplataforma, de código abierto, para la capa del servidor basado en el lenguaje de programación JavaScript.",
+  },
+  {
+    img: "../../../../../assets/img/icons8-express-js.svg",
+    title: "Express",
+    description:
+      "Express es un entorno de trabajo para aplicaciones web para el programario Node.js, de código abierto y con licencia MIT. Se utiliza para desarrollar aplicaciones web y APIs.",
+  },
+  {
+    img: "../../../../../assets/img/php-svgrepo-com.svg",
+    title: "PHP",
+    description:
+      "PHP es un lenguaje de programación de alto nivel de uso general que se adapta especialmente al desarrollo web.",
+  },
+  {
+    img: "../../../../../assets/img/icons8-postgresql.svg",
+    title: "PostgreSQL",
+    description:
+      "PostgreSQL es un sistema gestor de base de datos relacional de código abierto.",
+  },
+  {
+    img: "../../../../../assets/img/icons8-logo-de-mysql.svg",
+    title: "MySQL",
+    description:
+      "MySQL es un sistema gestor de base de datos relacional de código abierto.",
+  },
+];
+
+const mostrarDatos = (tecnologias) => {
+  const cards = document.getElementById("cardContainer");
+  const templateCards = document.getElementById("templateCard").content;
+  const fragment = document.createDocumentFragment();
+
+  tecnologias.map((item) => {
+    const clone = templateCards.cloneNode(true);
+
+    clone.querySelector("img").setAttribute("src", item.img);
+    clone.querySelector(".skills__card--content_title").textContent =
+      item.title;
+    clone.querySelector(".skills__card--content_description").textContent =
+      item.description;
+
+    fragment.appendChild(clone);
+  });
+  cards.appendChild(fragment);
+};
+
+mostrarDatos(tecnologias);
